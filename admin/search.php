fd82afba2123dc400d01e97708e771cba49eba15
Motyl <tmotyl@wakacje.pl>
2021-02-06 23:10:34 +0100

    Poprawienie walidacji zalogowania na search.php

--- admin/search.php
+++ admin/search.php
@@ -1,8 +1,9 @@
 <?php
-  require_once __DIR__ . '/../template/parts/head.php';
   require_once __DIR__ . '/src/autoload.php';
 
   AuthorizationChecker::check();
+
+  require_once __DIR__ . '/../template/parts/head.php';
 
   $filterService = new FilterService();
   $searchService = new SearchService();
