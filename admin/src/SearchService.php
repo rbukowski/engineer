ea707eb455eb277d8b11cea9011c598f4e2ccdec
Motyl <tmotyl@wakacje.pl>
2021-02-06 14:22:46 +0100

    Dzialajaca wyszukiwarka

--- admin/src/SearchService.php
+++ admin/src/SearchService.php
@@ -14,7 +14,53 @@ class SearchService
         $this->pdo = $pdo;
     }
 
-    public function searchConferenceRooms(): array
+    public function searchByType(string $type, array $filterTypeIds = []): array
+    {
+        $result = $this->getResultByType($type);
+
+        // Jak nie ma filtrów to zwracamy cały wynik.
+        if (empty($filterTypeIds)) {
+            return $result;
+        }
+
+        foreach ($result as $key => $row) {
+            // ArrayIntersect zwraca elementy pierwszej tablicy, które istnieją w drugiej tablicy.
+            // Sprawdzamy w ten sposób (poprzez zliczenie elementów pasujących do filtrów) i zliczenie filtrów
+            // Czy wszystkie filtry na pewno zostały spełnione.
+            // W takim przypadku, jeżeli przeszukiwany element będzie spełniał dodatkowe filtry - nie zostanie
+            // On wykluczony z wyniku.
+            $areFiltersSatisfied = count(
+                array_intersect($filterTypeIds, array_keys($row['types']))
+            ) === count($filterTypeIds);
+
+            if (!$areFiltersSatisfied) {
+                unset($result[$key]);
+            }
+        }
+
+        // Robię array_merge, żeby po stronie JavaScript mieć tablicę, a nie obiekty.
+        return array_merge($result);
+    }
+
+    private function getResultByType(string $type): array
+    {
+        switch ($type) {
+            case 'apartments':
+                return $this->searchApartments();
+
+            case 'rooms':
+                return $this->searchRooms();
+
+            case 'conference-rooms':
+                return $this->searchConferenceRooms();
+        }
+
+        throw new RuntimeException(
+            'Nieobsługiwany parametr wybierania filtrów!'
+        );
+    }
+
+    private function searchConferenceRooms(): array
     {
         $query = $this->pdo->prepare(<<<SQL
             SELECT
@@ -35,23 +81,30 @@ class SearchService
 
         $query->execute([]);
 
-        return array_map(
-            function (array $conferenceRoom): array {
-                $conferenceRoom['types'] = json_decode($conferenceRoom['types'], true);
-
-                return $conferenceRoom;
-            },
+        return $this->decodeTypes(
             $query->fetchAll(PDO::FETCH_ASSOC)
         );
     }
 
-    public function searchRooms(): array
+    private function searchRooms(): array
     {
         return [];
     }
 
-    public function searchApartments(): array
+    private function searchApartments(): array
     {
         return [];
+    }
+
+    private function decodeTypes(array $rows): array
+    {
+        return array_map(
+            function (array $row): array {
+                $row['types'] = json_decode($row['types'] ?? '{}', true);
+
+                return $row;
+            },
+            $rows
+        );
     }
 }
