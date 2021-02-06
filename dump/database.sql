e62747c2416f1f3ac04be5f326a08cfff1e78fa5
Motyl <tmotyl@wakacje.pl>
2021-02-06 14:41:13 +0100

    Zmiana nazwy bazy z database na inz

--- dump/database.sql
+++ dump/database.sql
@@ -8,8 +8,8 @@ create table apartments
 )
   collate = utf8_bin;
 
-INSERT INTO `database`.apartments (id, name, price, photo_url) VALUES (2, '353', 55, '../asset/offer/apartments/6.jpg');
-INSERT INTO `database`.apartments (id, name, price, photo_url) VALUES (3, 'źółć', 335, '../asset/offer/apartments/3.jpg');
+INSERT INTO `inz`.apartments (id, name, price, photo_url) VALUES (2, '353', 55, '../asset/offer/apartments/6.jpg');
+INSERT INTO `inz`.apartments (id, name, price, photo_url) VALUES (3, 'źółć', 335, '../asset/offer/apartments/3.jpg');
 
 create table apartments_relations
 (
@@ -19,8 +19,8 @@ create table apartments_relations
   apartment_type_id int null
 );
 
-INSERT INTO `database`.apartments_relations (id, apartment_id, apartment_type_id) VALUES (1, 2, 3);
-INSERT INTO `database`.apartments_relations (id, apartment_id, apartment_type_id) VALUES (2, 3, 2);
+INSERT INTO `inz`.apartments_relations (id, apartment_id, apartment_type_id) VALUES (1, 2, 3);
+INSERT INTO `inz`.apartments_relations (id, apartment_id, apartment_type_id) VALUES (2, 3, 2);
 
 create table apartments_type_groups
 (
@@ -29,7 +29,7 @@ create table apartments_type_groups
   name text null
 );
 
-INSERT INTO `database`.apartments_type_groups (id, name) VALUES (1, 'Rodzaj');
+INSERT INTO `inz`.apartments_type_groups (id, name) VALUES (1, 'Rodzaj');
 
 create table apartments_types
 (
@@ -40,10 +40,10 @@ create table apartments_types
 )
   collate = utf8_bin;
 
-INSERT INTO `database`.apartments_types (id, type, group_id) VALUES (1, 'Dla dwojga', 1);
-INSERT INTO `database`.apartments_types (id, type, group_id) VALUES (2, 'Z mini barem', 1);
-INSERT INTO `database`.apartments_types (id, type, group_id) VALUES (3, 'Królewski', 1);
-INSERT INTO `database`.apartments_types (id, type, group_id) VALUES (4, 'Książęcy', 1);
+INSERT INTO `inz`.apartments_types (id, type, group_id) VALUES (1, 'Dla dwojga', 1);
+INSERT INTO `inz`.apartments_types (id, type, group_id) VALUES (2, 'Z mini barem', 1);
+INSERT INTO `inz`.apartments_types (id, type, group_id) VALUES (3, 'Królewski', 1);
+INSERT INTO `inz`.apartments_types (id, type, group_id) VALUES (4, 'Książęcy', 1);
 
 create table conference_room_relations
 (
@@ -53,17 +53,17 @@ create table conference_room_relations
   conference_type_id int null
 );
 
-INSERT INTO `database`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (1, 5, 3);
-INSERT INTO `database`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (2, 5, 2);
-INSERT INTO `database`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (3, 5, 13);
-INSERT INTO `database`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (4, 1, 3);
-INSERT INTO `database`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (5, 1, 1);
-INSERT INTO `database`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (6, 1, 13);
-INSERT INTO `database`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (7, 2, 13);
-INSERT INTO `database`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (8, 3, 3);
-INSERT INTO `database`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (9, 3, 1);
-INSERT INTO `database`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (10, 3, 13);
-INSERT INTO `database`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (11, 3, 7);
+INSERT INTO `inz`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (1, 5, 3);
+INSERT INTO `inz`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (2, 5, 2);
+INSERT INTO `inz`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (3, 5, 13);
+INSERT INTO `inz`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (4, 1, 3);
+INSERT INTO `inz`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (5, 1, 1);
+INSERT INTO `inz`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (6, 1, 13);
+INSERT INTO `inz`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (7, 2, 13);
+INSERT INTO `inz`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (8, 3, 3);
+INSERT INTO `inz`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (9, 3, 1);
+INSERT INTO `inz`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (10, 3, 13);
+INSERT INTO `inz`.conference_room_relations (id, conference_room_id, conference_type_id) VALUES (11, 3, 7);
 
 create table conference_rooms
 (
@@ -75,11 +75,11 @@ create table conference_rooms
 )
   collate = utf8_bin;
 
-INSERT INTO `database`.conference_rooms (id, name, price, photo_url) VALUES (1, 'Pierwszy', 10, 'costam');
-INSERT INTO `database`.conference_rooms (id, name, price, photo_url) VALUES (2, 'Drugi', 20, 'costam');
-INSERT INTO `database`.conference_rooms (id, name, price, photo_url) VALUES (3, 'Trzeci', 30, 'costam');
-INSERT INTO `database`.conference_rooms (id, name, price, photo_url) VALUES (4, 'Czwarty', 40, 'costam');
-INSERT INTO `database`.conference_rooms (id, name, price, photo_url) VALUES (5, '353', 88, '../asset/offer/rooms/7.jpg');
+INSERT INTO `inz`.conference_rooms (id, name, price, photo_url) VALUES (1, 'Pierwszy', 10, 'costam');
+INSERT INTO `inz`.conference_rooms (id, name, price, photo_url) VALUES (2, 'Drugi', 20, 'costam');
+INSERT INTO `inz`.conference_rooms (id, name, price, photo_url) VALUES (3, 'Trzeci', 30, 'costam');
+INSERT INTO `inz`.conference_rooms (id, name, price, photo_url) VALUES (4, 'Czwarty', 40, 'costam');
+INSERT INTO `inz`.conference_rooms (id, name, price, photo_url) VALUES (5, '353', 88, '../asset/offer/rooms/7.jpg');
 
 create table conference_type_groups
 (
@@ -88,10 +88,10 @@ create table conference_type_groups
   name text null
 );
 
-INSERT INTO `database`.conference_type_groups (id, name) VALUES (1, 'Ilość osób');
-INSERT INTO `database`.conference_type_groups (id, name) VALUES (2, 'Rzutnik');
-INSERT INTO `database`.conference_type_groups (id, name) VALUES (3, 'Toaleta na sali konferencyjnej');
-INSERT INTO `database`.conference_type_groups (id, name) VALUES (4, 'Rodzaj siedzeń');
+INSERT INTO `inz`.conference_type_groups (id, name) VALUES (1, 'Ilość osób');
+INSERT INTO `inz`.conference_type_groups (id, name) VALUES (2, 'Rzutnik');
+INSERT INTO `inz`.conference_type_groups (id, name) VALUES (3, 'Toaleta na sali konferencyjnej');
+INSERT INTO `inz`.conference_type_groups (id, name) VALUES (4, 'Rodzaj siedzeń');
 
 create table conference_types
 (
@@ -102,19 +102,19 @@ create table conference_types
 )
   collate = utf8_bin;
 
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (1, 'Do 10 osób', 1);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (2, 'Do 25 osób', 1);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (3, 'Do 50 osób', 1);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (4, 'Do 100 osób', 1);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (5, 'Do 250 osób', 1);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (6, 'Do 500 osób', 1);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (7, 'Ma rzutnik', 2);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (8, 'Nie ma rzutnika', 2);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (10, 'Jest toaleta', 3);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (11, 'Nie ma tolety', 3);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (12, 'Do 750 osob', 1);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (13, 'Skórzane fotele', 4);
-INSERT INTO `database`.conference_types (id, type, group_id) VALUES (14, 'Drewniane krzesła', 4);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (1, 'Do 10 osób', 1);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (2, 'Do 25 osób', 1);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (3, 'Do 50 osób', 1);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (4, 'Do 100 osób', 1);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (5, 'Do 250 osób', 1);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (6, 'Do 500 osób', 1);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (7, 'Ma rzutnik', 2);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (8, 'Nie ma rzutnika', 2);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (10, 'Jest toaleta', 3);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (11, 'Nie ma tolety', 3);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (12, 'Do 750 osob', 1);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (13, 'Skórzane fotele', 4);
+INSERT INTO `inz`.conference_types (id, type, group_id) VALUES (14, 'Drewniane krzesła', 4);
 
 create table reservation_types
 (
@@ -147,7 +147,7 @@ create table rooms
 )
   collate = utf8_bin;
 
-INSERT INTO `database`.rooms (id, name, price, photo_url) VALUES (12, 'zamoreł', 45, '../asset/offer/rooms/2.jpg');
+INSERT INTO `inz`.rooms (id, name, price, photo_url) VALUES (12, 'zamoreł', 45, '../asset/offer/rooms/2.jpg');
 
 create table rooms_relations
 (
@@ -157,7 +157,7 @@ create table rooms_relations
   room_type_id int null
 );
 
-INSERT INTO `database`.rooms_relations (id, room_id, room_type_id) VALUES (1, 12, 1);
+INSERT INTO `inz`.rooms_relations (id, room_id, room_type_id) VALUES (1, 12, 1);
 
 create table rooms_type_groups
 (
@@ -166,7 +166,7 @@ create table rooms_type_groups
   name text null
 );
 
-INSERT INTO `database`.rooms_type_groups (id, name) VALUES (1, 'Ilość osób');
+INSERT INTO `inz`.rooms_type_groups (id, name) VALUES (1, 'Ilość osób');
 
 create table rooms_types
 (
@@ -177,10 +177,10 @@ create table rooms_types
 )
   collate = utf8_bin;
 
-INSERT INTO `database`.rooms_types (id, type, group_id) VALUES (1, 'Jednoosobowy', 1);
-INSERT INTO `database`.rooms_types (id, type, group_id) VALUES (2, 'Dwuosobowy', 1);
-INSERT INTO `database`.rooms_types (id, type, group_id) VALUES (3, 'Trzyosobowy', 1);
-INSERT INTO `database`.rooms_types (id, type, group_id) VALUES (4, 'Czteroosobowy', 1);
+INSERT INTO `inz`.rooms_types (id, type, group_id) VALUES (1, 'Jednoosobowy', 1);
+INSERT INTO `inz`.rooms_types (id, type, group_id) VALUES (2, 'Dwuosobowy', 1);
+INSERT INTO `inz`.rooms_types (id, type, group_id) VALUES (3, 'Trzyosobowy', 1);
+INSERT INTO `inz`.rooms_types (id, type, group_id) VALUES (4, 'Czteroosobowy', 1);
 
 create table users
 (
@@ -191,4 +191,4 @@ create table users
 )
   collate = utf8_bin;
 
-INSERT INTO `database`.users (id, name, password) VALUES (1, 'admin', '1841ff7291b3b51d50e5557acf7e8db67983b0ceabc293b66a55de7330eae5a3f1c88d75092f44d7daac2f084bf0e3f22d57c1cf050887c6fa27661422106111');
+INSERT INTO `inz`.users (id, name, password) VALUES (1, 'admin', '1841ff7291b3b51d50e5557acf7e8db67983b0ceabc293b66a55de7330eae5a3f1c88d75092f44d7daac2f084bf0e3f22d57c1cf050887c6fa27661422106111');
