9616ccff3cd51bd5a2f56e740ecba2d2009ec857
Motyl <tmotyl@wakacje.pl>
2021-02-06 19:26:01 +0100

    Obsluga bledow zakodowania odpowiedzi

--- admin/src/ResponseEmitter.php
+++ admin/src/ResponseEmitter.php
@@ -9,14 +9,26 @@ class ResponseEmitter
         $encodedBody = json_encode($body);
 
         ob_clean();
+
+        if (json_last_error()) {
+            http_response_code(500);
+            header('Content-Type: text/html; charset=UTF-8');
+
+            self::shutdown('Nie udało się zakodować odpowiedzi: ' . json_last_error_msg() . PHP_EOL);
+        }
+
         http_response_code($responseCode);
 
         header('Content-Type: application/json');
         header('Content-Length: ' . strlen($encodedBody));
         header('Cache-Control: no-cache');
 
-        echo $encodedBody;
+        self::shutdown($encodedBody);
+    }
 
-        exit(0);
+    private static function shutdown(?string $output = null): void
+    {
+        echo $output;
+        exit;
     }
 }
