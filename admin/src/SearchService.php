d3c94d766813664ec4e5e4771456f84b4eba783f
Motyl <tmotyl@wakacje.pl>
2021-02-06 14:37:27 +0100

    Podlaczenie pozostalych wyszukiwarek

--- admin/src/SearchService.php
+++ admin/src/SearchService.php
@@ -91,12 +91,54 @@ class SearchService
 
     private function searchRooms(): array
     {
-        return [];
+        $query = $this->pdo->prepare(<<<SQL
+            SELECT
+              r.*,
+              COALESCE(
+                JSON_OBJECTAGG(rt.id, rt.type),
+                '{}'
+              ) AS types
+            FROM
+              rooms AS r
+            LEFT JOIN
+              rooms_relations AS rr ON rr.room_id = r.id
+            LEFT JOIN
+              rooms_types AS rt ON rt.id = rr.room_type_id
+            GROUP BY
+              r.id
+        SQL);
+
+        $query->execute([]);
+
+        return $this->decodeTypes(
+            $query->fetchAll(PDO::FETCH_ASSOC)
+        );
     }
 
     private function searchApartments(): array
     {
-        return [];
+        $query = $this->pdo->prepare(<<<SQL
+            SELECT
+              a.*,
+              COALESCE(
+                JSON_OBJECTAGG(at.id, at.type),
+                '{}'
+              ) AS types
+            FROM
+              apartments AS a
+            LEFT JOIN
+              apartments_relations AS ar ON ar.apartment_id = a.id
+            LEFT JOIN
+              apartments_types AS at ON at.id = ar.apartment_type_id
+            GROUP BY
+              a.id
+        SQL);
+
+        $query->execute([]);
+
+        return $this->decodeTypes(
+            $query->fetchAll(PDO::FETCH_ASSOC)
+        );
     }
 
     private function decodeTypes(array $rows): array
