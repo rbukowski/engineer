5ba3cd5d9c83d2c995b6da7e7e9cf7c756041327
Motyl <tmotyl@wakacje.pl>
2021-02-06 19:22:28 +0100

    Ustawianie naglowkow dla przegladarki

--- admin/src/ResponseEmitter.php
+++ admin/src/ResponseEmitter.php
@@ -6,8 +6,17 @@ class ResponseEmitter
 {
     public static function emit(array $body, int $responseCode = 200): void
     {
+        $encodedBody = json_encode($body);
+
+        ob_clean();
         http_response_code($responseCode);
-        echo json_encode($body);
+
+        header('Content-Type: application/json');
+        header('Content-Length: ' . strlen($encodedBody));
+        header('Cache-Control: no-cache');
+
+        echo $encodedBody;
+
         exit(0);
     }
 }
