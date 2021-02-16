b58936d022525c8981bbb91df8a1b359209e9904
pzamorski <przemyslaw.zamorski@stxnext.pl>
2021-02-16 18:56:50 +0100

    remove authorization checker on search

--- admin/search.php
+++ admin/search.php
@@ -1,8 +1,5 @@
 <?php
   require_once __DIR__ . '/src/autoload.php';
-
-  AuthorizationChecker::check();
-
   require_once __DIR__ . '/../template/parts/head.php';
 
   $filterService = new FilterService();
