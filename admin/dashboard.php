a171cf61f9228c143193d160629a290bb54d4877
pzamorski <przemyslaw.zamorski@stxnext.pl>
2021-02-17 21:02:36 +0100

    dashboard.php -  add head import. add todo for motyl

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,7 +1,8 @@
 <?php
-    require_once __DIR__ . '/src/autoload.php';
+    require_once __DIR__ . '/../template/parts/head.php';
 
-    AuthorizationChecker::check();
+    // TODO: motyl dlaczego to się wywala jak sie odkomentuje po imporcie heada
+    // AuthorizationChecker::check();
 
     $dashboardService = new DashboardService();
 
@@ -9,24 +10,6 @@
     $apartments = $dashboardService->getApartmentsOnAdminDashboard();
     $conferenceRooms = $dashboardService->getConferenceOnAdminDashboard();
 ?>
-
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/fontawesome/css/all.css" rel="stylesheet">
-
-
-    <title>Raport</title>
-</head>
 
 <body>
     <div class="container-fluid bg-dark p-3">
